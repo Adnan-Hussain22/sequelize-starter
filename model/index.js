2abe8e094b76304dfe33f8ec117f6227978187f5
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-04-19 01:36:08 +0500

    model validations

--- model/index.js
+++ model/index.js
@@ -6,16 +6,31 @@ const User = connection.define(
     uuid: {
       type: Sequelize.UUID,
       primaryKey: true,
-      defaultValue: Sequelize.UUIDV4,
       //If you do not assign this default value than uuid will
       // be null in case no uuid provided
+      defaultValue: Sequelize.UUIDV4,
     },
-    name: Sequelize.STRING,
-    bio: Sequelize.TEXT,
+    name: {
+      type: Sequelize.STRING,
+      validate: {
+        len: {
+          args: [2], // len: [minLength, maxLength]
+          msg: "Error: Name length should be greater than 2",
+        },
+      },
+    },
+    bio: {
+      type: Sequelize.TEXT, // For long string values
+      validate: {
+        contains: {
+          args: ["hello world"], //xD
+          msg: "Error: bio should contain 'hello world'",
+        },
+      },
+    },
   },
   {
-    timestamps: false // when you do not want sequelize to add createAt and updatedAt fields automatically
-
+    timestamps: false, // when you do not want sequelize to add createAt and updatedAt fields automatically
   }
 );
 module.exports = { User };
