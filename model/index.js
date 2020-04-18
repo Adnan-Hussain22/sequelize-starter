b79e0f9c48bfb890ff8fc95ac4b578fb5258ccd2
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-04-19 00:30:07 +0500

    added primary to user model and create one user

--- model/index.js
+++ model/index.js
@@ -1,6 +1,13 @@
 const Sequelize = require("sequelize");
-const { CONNECTION } = require("../config");
-const User = CONNECTION.define("User", {
+const { connection } = require("../config");
+const User = connection.define("User", {
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+    //If you do not assign this default value than uuid will
+    // be null in case no uuid provided
+  },
   name: Sequelize.STRING,
   bio: Sequelize.TEXT,
 });
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const { connection } = require("./config");
+const {User} = require('./model')
 const app = express();
 const PORT = 5000;
 
 connection
-  .authenticate({
+  .sync({ // sync is used to create table automatically by sequelize
     logging: console.log,
+    force: true // Drops the table if it already exist
+    // do this only when you know what you are doing or when you have changed the schema
+  })
+  .then(()=> {
+    User.create({
+      name: 'Adnan Rajput',
+      bio: 'Playing with sequelize and sqlite xD'
+    })
   })
   .then(() => console.log("SQLite connected..."))
   .catch((err) => console.log("Error while connecting SQLite==>", err));
