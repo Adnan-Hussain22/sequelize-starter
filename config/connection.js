e21e32ec6404fb62f2a8d04c5fb73187f572f084
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-04-19 00:41:29 +0500

    modified env

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize");
 const { config } = require("./env");
-const connection = new Sequelize("db", "user", "pass", {
-  host: config.HOST,
-  dialect: config.DIALECT,
-  storage: config.STORAGE,
-  operatorsAliases: 0,
-});
+const connection = new Sequelize(
+  config.DATABASE,
+  config.USERNAME,
+  config.PASSWORD,
+  {
+    host: config.HOST,
+    dialect: config.DIALECT,
+    storage: config.STORAGE,
+    operatorsAliases: 0,
+  }
+);
 module.exports = { connection };
--- config/env.js
+++ config/env.js
@@ -3,6 +3,9 @@ const config = {
   HOST: process.env.HOST,
   DIALECT: process.env.DIALECT,
   STORAGE: process.env.STORAGE,
+  DATABASE: process.env.DATABASE,
+  USERNAME: process.env.USERNAME,
+  PASSWORD: process.env.PASSWORD
 };
 console.log({config})
 module.exports = { config };
