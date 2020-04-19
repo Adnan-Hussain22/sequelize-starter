22910ba27fd0aaf52716eba905626bfa9492daa1
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-04-19 22:55:48 +0500

    comments api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { UserRouter, PostRouter } = require("./routes");
+const { UserRouter, PostRouter, CommentRouter } = require("./routes");
 const app = express();
 const PORT = 5000;
 
@@ -8,5 +8,6 @@ app.use(bodyParser.json()); //parse the string body
 
 app.use("/api/user", UserRouter);
 app.use("/api/post", PostRouter);
+app.use("/api/comment", CommentRouter);
 
 app.listen(PORT, () => console.log("Server listening on PORT " + PORT));
