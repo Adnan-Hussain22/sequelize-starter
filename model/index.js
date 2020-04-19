31f4169c6396e38391d28b1bc5a219e1e6043b29
Adnan-Hussain22 <adnanrajput42@gmail.com>
2020-04-19 22:54:21 +0500

    Add associations, 1. Coment to User (One-to-One) 2. Post to many comments (One-to-Many)

--- model/index.js
+++ model/index.js
@@ -1,11 +1,27 @@
 const { Post } = require("./post");
 const { User } = require("./user");
+const { Comment } = require("./comment");
+
+/*One-To-One Association*/
 Post.belongsTo(User, {
   foreignKey: "userId",
   // as is used to aliase a master table while populating it in detail
   as: "user",
 });
+Comment.belongsTo(User, {
+  foreignKey: { name: "userId", allowNull: false },
+  as: "user",
+});
+
+/*One-To-Many Association*/
+Post.hasMany(Comment, {
+  foreignKey: { name: "postId", allowNull: false },
+  as: "comments",
+  foreignKeyConstraint: true,
+});
+
 module.exports = {
-  Post,
   User,
+  Post,
+  Comment,
 };
